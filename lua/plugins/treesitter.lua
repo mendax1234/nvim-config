b3255bac36d32e5921bc0c2feba07d03080ef435
mendax1234 <daniel.wb.zhu@gmail.com>
2024-03-17 10:00:23 +0800

    Add different language parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,23 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = {'lua', 'javascript'},
+            ensure_installed = {
+                'lua',
+                'javascript',
+                'python',
+                'html',
+                'css',
+                'json',
+                'bash',
+                'yaml',
+                'typescript',
+                'c',
+                'cpp',
+                'csv',
+                'go',
+                'rust',
+                'xml',
+            },
             highlight = { enable = true },
             indent = { enable = true },
         })
