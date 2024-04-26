14937f771aaea90213f78c72d2bbb722dc2e7429
mendax1234 <daniel.wb.zhu@gmail.com>
2024-04-26 15:55:22 +0800

    Change tab space and add c debugger

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -25,9 +25,46 @@ return {
 			dapui.close()
 		end
 
-		vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+    -- Cpp debugger setup
+    dap.adapters.cppdbg = {
+        id = 'cppdbg',
+        type = 'executable',
+        command = '/home/wenbo/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
+    }
+
+    -- Cpp debugger configuration
+    dap.configurations.cpp = {
+      {
+        name = "Launch file",
+        type = "cppdbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopAtEntry = true,
+      },
+      {
+        name = 'Attach to gdbserver :1234',
+        type = 'cppdbg',
+        request = 'launch',
+        MIMode = 'gdb',
+        miDebuggerServerAddress = 'localhost:1234',
+        miDebuggerPath = '/usr/bin/gdb',
+        cwd = '${workspaceFolder}',
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+      },
+    }
+
+    -- Set C++ configuration to C
+    dap.configurations.c = dap.configurations.cpp
+
+    vim.keymap.set("n", "<Leader>dt", ":DapUiToggle<CR>")
+    vim.keymap.set("n", "<Leader>db", ":DapToggleBreakpoint<CR>")
 		vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
 		vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
 		vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
-	end,
+end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,28 +1,28 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            ensure_installed = {
-                'lua',
-                'javascript',
-                'python',
-                'html',
-                'css',
-                'json',
-                'bash',
-                'yaml',
-                'typescript',
-                'c',
-                'cpp',
-                'csv',
-                'go',
-                'rust',
-                'xml',
-            },
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = {
+        'lua',
+        'javascript',
+        'python',
+        'html',
+        'css',
+        'json',
+        'bash',
+        'yaml',
+        'typescript',
+        'c',
+        'cpp',
+        'csv',
+        'go',
+        'rust',
+        'xml',
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
 }
