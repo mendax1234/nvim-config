89f14584e61a350c61e1f949263a3a8de3a70d58
mendax1234 <daniel.wb.zhu@gmail.com>
2024-04-27 21:41:01 +0800

    Add nvim-nio to suit the latest nvim-dap-ui

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,70 +1,73 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"leoluz/nvim-dap-go",
-		"rcarriga/nvim-dap-ui",
-    "mfussenegger/nvim-dap-python",
-	},
-  config = function()
-  require("dapui").setup()
-  require("dap-go").setup()
-  require("dap-python").setup('~/.virtualenvs/debugpy/bin/python')
-
-  local dap, dapui = require("dap"), require("dapui")
-
-  dap.listeners.before.attach.dapui_config = function()
-    dapui.open()
-  end
-  dap.listeners.before.launch.dapui_config = function()
-    dapui.open()
-  end
-  dap.listeners.before.event_terminated.dapui_config = function()
-    dapui.close()
-  end
-  dap.listeners.before.event_exited.dapui_config = function()
-    dapui.close()
-  end
-
-  -- Cpp debugger setup
-  dap.adapters.cppdbg = {
-    id = 'cppdbg',
-    type = 'executable',
-    command = '/home/wenbo/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
-  }
-
-  -- Cpp debugger configuration
-  dap.configurations.cpp = {
-    {
-      name = "Launch file",
-      type = "cppdbg",
-      request = "launch",
-      program = function()
-          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-      end,
-      cwd = '${workspaceFolder}',
-      stopAtEntry = true,
+  { "nvim-neotest/nvim-nio" },
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "leoluz/nvim-dap-go",
+      "rcarriga/nvim-dap-ui",
+      "mfussenegger/nvim-dap-python",
     },
-    {
-      name = 'Attach to gdbserver :1234',
-      type = 'cppdbg',
-      request = 'launch',
-      MIMode = 'gdb',
-      miDebuggerServerAddress = 'localhost:1234',
-      miDebuggerPath = '/usr/bin/gdb',
-      cwd = '${workspaceFolder}',
-      program = function()
-          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-      end,
-    },
+    config = function()
+    require("dapui").setup()
+    require("dap-go").setup()
+    require("dap-python").setup('~/.virtualenvs/debugpy/bin/python')
+
+    local dap, dapui = require("dap"), require("dapui")
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    -- Cpp debugger setup
+    dap.adapters.cppdbg = {
+      id = 'cppdbg',
+      type = 'executable',
+      command = '/home/wenbo/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
+    }
+
+    -- Cpp debugger configuration
+    dap.configurations.cpp = {
+      {
+        name = "Launch file",
+        type = "cppdbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopAtEntry = true,
+      },
+      {
+        name = 'Attach to gdbserver :1234',
+        type = 'cppdbg',
+        request = 'launch',
+        MIMode = 'gdb',
+        miDebuggerServerAddress = 'localhost:1234',
+        miDebuggerPath = '/usr/bin/gdb',
+        cwd = '${workspaceFolder}',
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+      },
+    }
+
+    -- Set C++ configuration to C
+    dap.configurations.c = dap.configurations.cpp
+
+    -- keymap
+    vim.keymap.set("n", "<Leader>db", ":DapToggleBreakpoint<CR>")
+    vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+    vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+    vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+  end,
   }
-
-  -- Set C++ configuration to C
-  dap.configurations.c = dap.configurations.cpp
-
-  -- keymap
-  vim.keymap.set("n", "<Leader>db", ":DapToggleBreakpoint<CR>")
-  vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
-  vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-  vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
-end,
 }
